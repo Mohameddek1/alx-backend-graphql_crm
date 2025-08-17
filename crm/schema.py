743a2dbbcc3312a36dc211f39129250df6fbd495
Mohameddek1 <mohadek2004@gmail.com>
2025-08-17 14:39:43 +0300

    first commit

--- crm/schema.py
+++ crm/schema.py
@@ -1,6 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 from .models import Customer, Product, Order
+from .filters import CustomerFilter, ProductFilter, OrderFilter
 from django.db import transaction
 from django.core.exceptions import ValidationError
 from django.utils import timezone
@@ -117,6 +119,9 @@ class Query(graphene.ObjectType):
     customers = graphene.List(CustomerType)
     products = graphene.List(ProductType)
     orders = graphene.List(OrderType)
+    all_customers = DjangoFilterConnectionField(CustomerType, filterset_class=CustomerFilter)
+    all_products = DjangoFilterConnectionField(ProductType, filterset_class=ProductFilter)
+    all_orders = DjangoFilterConnectionField(OrderType, filterset_class=OrderFilter)
     def resolve_customers(self, info):
         return Customer.objects.all()
     def resolve_products(self, info):
